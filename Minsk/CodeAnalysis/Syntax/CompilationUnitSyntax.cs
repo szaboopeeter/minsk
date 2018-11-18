3d99f311192f45ad783a8ffa5a212874182474f3
Péter Szabó <szaboopeeter@live.com>
2018-11-18 15:13:02 +0700

    Compilation unit

---
+++ Minsk/CodeAnalysis/Syntax/CompilationUnitSyntax.cs
@@ -0,0 +1,11 @@
+﻿namespace Minsk.CodeAnalysis.Syntax
+{
+    public sealed class CompilationUnitSyntax : SyntaxNode
+    {
+        public CompilationUnitSyntax(ExpressionSyntax exression, SyntaxToken endOfFileToken) => Exression = exression;
+
+        public ExpressionSyntax Exression { get; }
+
+        public override SyntaxKind Kind => SyntaxKind.CompilationUnit;
+    }
+}
