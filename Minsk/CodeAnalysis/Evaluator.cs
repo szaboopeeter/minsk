26e785b2e1c13d4d6ea875bd9a3ca9223b9bf274
Péter Szabó <szaboopeeter@live.com>
2018-12-23 18:48:56 +0100

    Lower for statement into while statement

--- Minsk/CodeAnalysis/Evaluator.cs
+++ Minsk/CodeAnalysis/Evaluator.cs
@@ -43,22 +43,8 @@ namespace Minsk.CodeAnalysis
                 case BoundNodeKind.VariableDeclaration:
                     EvaluateVariableDeclaration((BoundVariableDeclaration)node);
                     break;
-                case BoundNodeKind.ForStatement:
-                    EvaluateForStatement((BoundForStatement)node);
-                    break;
                 default:
                     throw new Exception($"Unexpected node {node.Kind}");
-            }
-        }
-
-        private void EvaluateForStatement(BoundForStatement node)
-        {
-            var lowerBound = (int)EvaluateExpression(node.LowerBound);
-            var upperBound = (int)EvaluateExpression(node.UpperBound);
-            for (var i = lowerBound; i <= upperBound; i++)
-            {
-                _variables[node.Variable] = i;
-                EvaluateStatement(node.Body);
             }
         }
 
--- Minsk/CodeAnalysis/Lowering/Lowerer.cs
+++ Minsk/CodeAnalysis/Lowering/Lowerer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using Minsk.CodeAnalysis.Binding;
 
 namespace Minsk.CodeAnalysis.Lowering
@@ -5,13 +6,54 @@ namespace Minsk.CodeAnalysis.Lowering
     internal sealed class Lowerer : BoundTreeRewriter
     {
         private Lowerer()
-        {            
+        {
         }
 
         public static BoundStatement Lower(BoundStatement statement)
         {
             var lowerer = new Lowerer();
             return lowerer.RewriteStatement(statement);
+        }
+
+        protected override BoundStatement RewriteForStatement(BoundForStatement node)
+        {
+            // for <var> = <lower> to <upper>
+            //     <body>
+            //
+            // ---->
+            //
+            // {
+            //     var <var> = <lower>
+            //     while (<var> <= <upper>)
+            //     {
+            //         <body>
+            //         <var> = <var> + 1
+            //     }
+            // }
+
+            var variableDeclaration = new BoundVariableDeclaration(node.Variable, node.LowerBound);
+            var variableExpression = new BoundVariableExpression(node.Variable);
+            var condition = new BoundBinaryExpression(
+                variableExpression,
+                BoundBinaryOperator.Bind(Syntax.SyntaxKind.LessOrEqualsToken, typeof(int), typeof(int)),
+                node.UpperBound);
+
+            var increment = new BoundExpressionStatement(
+                new BoundAssignmentExpression(
+                    node.Variable,
+                    new BoundBinaryExpression(
+                        variableExpression,
+                        BoundBinaryOperator.Bind(Syntax.SyntaxKind.PlusToken, typeof(int), typeof(int)),
+                        new BoundLiteralExpression(1)
+                    )
+                )
+            );
+
+            var whileBody = new BoundBlockStatement(ImmutableArray.Create(node.Body, increment));
+            var whileStatement = new BoundWhileStatement(condition, whileBody);
+            var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(variableDeclaration, whileStatement));
+
+            return RewriteStatement(result);
         }
     }
 }
