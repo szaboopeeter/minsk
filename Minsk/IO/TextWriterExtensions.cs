9970af8c7aefcf4f5443c98c7b0b619c6d2720ed
Péter Szabó <szaboopeeter@live.com>
2020-04-05 14:21:42 +0700

    Add sample and compiler project

--- Minsk/IO/TextWriterExtensions.cs
+++ Minsk/IO/TextWriterExtensions.cs
@@ -1,11 +1,15 @@
 using System;
 using System.CodeDom.Compiler;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Minsk.CodeAnalysis;
 using Minsk.CodeAnalysis.Syntax;
+using Minsk.CodeAnalysis.Text;
 
 namespace Minsk.IO
 {
-    internal static class TextWriterExtensions
+    public static class TextWriterExtensions
     {
         private static bool IsConsoleOut(this TextWriter writer)
         {
@@ -86,6 +90,44 @@ namespace Minsk.IO
         public static void WritePunctuation(this TextWriter writer, SyntaxKind kind)
         {
             writer.WritePunctuation(SyntaxFacts.GetText(kind));
+        }
+
+        public static void WriteDiagnostics(this TextWriter writer, IEnumerable<Diagnostic> diagnostics, SyntaxTree syntaxTree)
+        {
+            foreach (var diagnostic in diagnostics.OrderBy(diag => diag.Span.Start).ThenBy(diag => diag.Span.Length))
+            {
+                var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+                var line = syntaxTree.Text.Lines[lineIndex];
+                var lineNumber = lineIndex + 1;
+                var character = diagnostic.Span.Start - line.Start + 1;
+
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write($"({lineNumber}, {character}): ");
+                Console.WriteLine(diagnostic);
+                Console.ResetColor();
+
+                var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                var prefix = syntaxTree.Text.ToString(prefixSpan);
+                var error = syntaxTree.Text.ToString(diagnostic.Span);
+                var suffix = syntaxTree.Text.ToString(suffixSpan);
+
+                Console.Write("    ");
+                Console.Write(prefix);
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write(error);
+                Console.ResetColor();
+
+                Console.Write(suffix);
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine();
         }
     }
 }
---
+++ mc/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Minsk.CodeAnalysis;
+using Minsk.CodeAnalysis.Symbols;
+using Minsk.CodeAnalysis.Syntax;
+using Minsk.IO;
+
+namespace Minsk
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Usage is mc <source-paths>");
+                return;
+            }
+
+            if (args.Length > 1)
+            {
+                System.Console.WriteLine("Error: only one path supported right now.");
+            }
+
+            var path = args.Single();
+
+            var text = File.ReadAllText(path);
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+            if (result.Diagnostics.Any())
+            {
+                Console.Error.WriteDiagnostics(result.Diagnostics, syntaxTree);
+            }
+            else
+            {
+                if (result.Value != null)
+                {
+                    Console.WriteLine(result.Value);
+                }
+            }
+        }
+    }
+}
